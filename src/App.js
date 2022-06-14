456860a5bfc70e00259f302ed8f0e0b6ffd73501
Yuri Murik <72994373+YuriMurik@users.noreply.github.com>
2022-06-13 23:56:01 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import React, { Component } from "react";
+import './estilo.css'
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      textoFrase: ''
+    };
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+    this.frases = [
+      'A vida trará coisas boas se tiver paciência.',
+      'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.',
+      'Não compense na ira o que lhe falta na razão.',
+      'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+      'A maior de todas as torres começa no solo.',
+      'Não há que ser forte. Há que ser flexível.',
+    ]
+
+  }
+
+  quebraBiscoito() {
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
+    state.textoFrase = ' " ' + this.frases[numeroAleatorio] + ' " ';
+    this.setState(state);
+  }
 
   render() {
     return (
-      <div>
-      
+      <div className="container">
 
+        <img src={require('./assets/biscoito.png')} className='img' />
+        <Botao nome='Abrir biscoito' acaoBtn={this.quebraBiscoito} />
+        <h3 className="textoFrase">{this.state.textoFrase}</h3>
       </div>
     );
   }
+}
 
+class Botao extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
+      </div>
+    )
+  }
 }
 
 export default App;
